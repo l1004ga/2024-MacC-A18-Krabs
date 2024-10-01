3b2e2f31650b6a4cc0dbd3724444d7dcdd3d9df8
YunKi-H <yunki.dev@gmail.com>
2024-10-01 20:18:54 +0900

    [feat] #8 Init MetronomeOnOffUseCase

---
+++ Macro/Domain/UseCase/MetronomeOnOffUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  MetronomeOnOffUseCase.swift
+//  Macro
+//
+//  Created by Yunki on 10/1/24.
+//
+
+class MetronomeOnOffUseCase {
+    func play() {
+        
+    }
+    
+    func stop() {
+        
+    }
+}
