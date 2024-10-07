a6dd32c5d5d5a17433fc44b6b935cd1e6ea13147
jina <l1004ga@gmail.com>
2024-10-07 16:13:00 +0900

    [refator] #3 MoveNextAccentInterface 파일 분리

--- Macro/Domain/UseCase/AccentUseCase.swift
+++ Macro/Domain/UseCase/AccentUseCase.swift
@@ -18,10 +18,3 @@ extension AccentUseCase {
         templateUseCase.moveNextAccent(daebakIndex: daebakIndex, sobakIndex: sobakIndex)
     }
 }
-
-protocol MoveNextAccentInterface {
-    // 대박리스트의 각 박에 대하여 사용자가 탭을 누를 시 매개변수를 사용하여 해당 Index의 강세를 변경해줍니다.
-    func moveNextAccent(daebakIndex: Int, sobakIndex: Int)
-}
-
-
