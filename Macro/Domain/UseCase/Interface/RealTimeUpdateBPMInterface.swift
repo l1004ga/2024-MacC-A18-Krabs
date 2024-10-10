a31a21aa7429cee2a3ac83184e23baa049e58257
jina <l1004ga@gmail.com>
2024-10-10 17:14:00 +0900

    [feat] #28 실시간으로 사용자 조작 BPM을 반영하여 소리 재생 및 화면 변경

---
+++ Macro/Domain/UseCase/Interface/RealTimeUpdateBPMInterface.swift
@@ -0,0 +1,12 @@
+//
+//  RealTimeUpdateBPMInterface.swift
+//  Macro
+//
+//  Created by leejina on 10/10/24.
+//
+
+import Foundation
+
+protocol RealTimeUpdateBPMInterface {
+    func updateBPM(bpm: Int)
+}
