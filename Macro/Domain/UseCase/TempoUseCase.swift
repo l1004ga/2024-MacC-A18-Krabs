f5c5903bf2623f094733c40c9dc4ca03ca49c2bb
YunKi-H <yunki.dev@gmail.com>
2024-10-10 20:25:18 +0900

    [feat] #28 실시간 BPM 변경 구현 - 변경이 매끄럽게 이루어지지 않는 이슈 있음

--- Macro/Domain/UseCase/TempoUseCase.swift
+++ Macro/Domain/UseCase/TempoUseCase.swift
@@ -10,17 +10,13 @@ import Foundation
 
 class TempoUseCase {
     private var templateUseCase: UpdateTempoInterface
-    private var metronomeOnOffUseCase: RealTimeUpdateBPMInterface
     
-    init(templateUseCase: UpdateTempoInterface, metronomeOnOffUseCase: RealTimeUpdateBPMInterface) {
+    init(templateUseCase: UpdateTempoInterface) {
         self.templateUseCase = templateUseCase
-        self.metronomeOnOffUseCase = metronomeOnOffUseCase
     }
     
     func updateTempo(newBpm: Int) {
         // 사용자가 변경한 BPM을 각 장단별 데이터 객체에 저장 요청
         templateUseCase.updateTempo(newBpm: newBpm)
-        // 사용자가 변경한 BPM을 재생되는 박자 소리 및 화면 변경 과정의 interval 측정에 실시간 반영을 요청
-        metronomeOnOffUseCase.updateBPM(bpm: newBpm)
     }
 }
