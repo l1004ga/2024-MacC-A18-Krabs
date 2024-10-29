9c863bfbb22caccc1072f657ccb24b52badedc22
YunKi-H <yunki.dev@gmail.com>
2024-10-30 00:10:07 +0900

    [chore] #83 Accent 별도 파일로 분리

--- Macro/Domain/Entity/JangdanEntity.swift
+++ Macro/Domain/Entity/JangdanEntity.swift
@@ -16,30 +16,3 @@ struct JangdanEntity {
         var bakAccentList: [Accent]
     }
 }
-
-enum Accent {
-    case strong
-    case medium
-    case weak
-    case none
-    
-    func nextAccent() -> Accent {
-        switch self {
-        case .strong: return .medium
-        case .medium: return .weak
-        case .weak: return .none
-        case .none: return .strong
-        }
-    }
-}
-
-extension Accent: Comparable {
-    static func < (lhs: Accent, rhs: Accent) -> Bool {
-        switch (lhs, rhs) {
-        case (.none, .strong), (.weak, .strong), (.medium, .strong): return true
-        case (.none, .medium), (.weak, .medium): return true
-        case (.none, .weak): return true
-        default: return false
-        }
-    }
-}
