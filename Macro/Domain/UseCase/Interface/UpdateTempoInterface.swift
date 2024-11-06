5825efa107cc4f90172599755de87d2721bf5a6f
YunKi-H <yunki.dev@gmail.com>
2024-11-06 23:50:34 +0900

    [feat] #113 TemplateUseCase에서 데이터 관리 책임 분리

--- Macro/Domain/UseCase/Interface/UpdateTempoInterface.swift
+++ Macro/Domain/UseCase/Interface/UpdateTempoInterface.swift
@@ -7,7 +7,7 @@
 
 // 사용자가 설정한 bpm으로 업데이트 도와주는 UseCase용
 
-protocol UpdateTempoInterface {
-  
-    func updateTempo(newBpm: Int)
-}
+//protocol UpdateTempoInterface {
+//  
+//    func updateTempo(newBpm: Int)
+//}
