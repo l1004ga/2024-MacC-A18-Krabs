ecc265729e24f231c0b8fbc261d01cce304714ab
YunKi-H <yunki.dev@gmail.com>
2024-11-07 00:38:21 +0900

    [feat] #113 MetronomeOnOffUseCase에 JangdanRepository적용

--- Macro/Domain/UseCase/MetronomeOnOffUseCase.swift
+++ Macro/Domain/UseCase/MetronomeOnOffUseCase.swift
@@ -17,13 +17,11 @@ class MetronomeOnOffUseCase {
             return jangdan.map { $0.enumerated().map { $0.offset == 0 ? $0.element : .none }}.flatMap { $0 }
         }
     }
-    private var bpm: Double
-    private var currentBeat: Int
+    private var bpm: Int
+    private var currentBeatIndex: Int
     var isSobakOn: Bool
     
     var cancelBag: Set<AnyCancellable> = []
-    var jangdanSubscription: AnyCancellable?
-    var bpmSubscription: AnyCancellable?
     
     // timer
     private var timer: DispatchSourceTimer?
@@ -32,30 +30,26 @@ class MetronomeOnOffUseCase {
         60.0 / Double(bpm)
     }
     
-    private var templateUseCase: JangdanSelectInterface
+    private var jangdanRepository: JangdanRepository
     private var soundManager: PlaySoundInterface
     
-    init(templateUseCase: JangdanSelectInterface, soundManager: PlaySoundInterface) {
+    init(jangdanRepository: JangdanRepository, soundManager: PlaySoundInterface) {
         self.jangdan = [[.medium]]
         self.bpm = 60
-        self.currentBeat = 0
+        self.currentBeatIndex = 0
         self.isSobakOn = false
         
-        self.templateUseCase = templateUseCase
+        self.jangdanRepository = jangdanRepository
         self.soundManager = soundManager
         
-        self.jangdanSubscription = templateUseCase.jangdanPublisher.sink { [weak self] jangdan in
+        self.jangdanRepository.jangdanPublisher.sink { [weak self] jangdanEntity in
             guard let self else { return }
-            self.jangdan = jangdan
-        }
-        self.jangdanSubscription?.store(in: &self.cancelBag)
-        self.bpmSubscription = templateUseCase.bpmPublisher.sink { [weak self] bpm in
-            guard let self else { return }
-            self.bpm = bpm
-            // Timer의 schedule을 현재 변경된 bpm에 맞게 재설정
+            self.jangdan = jangdanEntity.daebakList.map { $0.bakAccentList }
+            
+            self.bpm = jangdanEntity.bpm
             self.timer?.schedule(deadline: .now() + self.interval, repeating: self.interval, leeway: .nanoseconds(1))
         }
-        self.bpmSubscription?.store(in: &self.cancelBag)
+        .store(in: &self.cancelBag)
     }
 }
 
@@ -65,33 +59,34 @@ extension MetronomeOnOffUseCase {
     func changeSobak() {
         self.isSobakOn.toggle()
     }
-        func play(_ tickHandler: @escaping () -> Void ) {
-            // 데이터 갱신
-            self.currentBeat = 0
+    
+    func play(_ tickHandler: @escaping () -> Void ) {
+        // 데이터 갱신
+        self.currentBeatIndex = 0
+        
+        // Timer 설정
+        if let timer { self.stop() }
+        self.timer = DispatchSource.makeTimerSource(queue: self.queue)
+        self.timer?.schedule(deadline: .now(), repeating: self.interval)
+        self.timer?.setEventHandler { [weak self] in
+            guard let self = self else { return }
             
-            // Timer 설정
-            if let timer { self.stop() }
-            self.timer = DispatchSource.makeTimerSource(queue: self.queue)
-            self.timer?.schedule(deadline: .now(), repeating: self.interval)
-            self.timer?.setEventHandler { [weak self] in
-                guard let self = self else { return }
-                
-                tickHandler()
-                self.timerHandler()
-            }
-            
-            // Timer 실행
-            self.timer?.resume()
+            tickHandler()
+            self.timerHandler()
         }
         
-        func stop() {
-            self.timer?.cancel()
-            self.timer = nil
-        }
-        
-        private func timerHandler() {
-            let accent: Accent = jangdanAccentList[self.currentBeat % jangdanAccentList.count]
-            self.soundManager.beep(accent)
-            self.currentBeat += 1
-        }
+        // Timer 실행
+        self.timer?.resume()
     }
+    
+    func stop() {
+        self.timer?.cancel()
+        self.timer = nil
+    }
+    
+    private func timerHandler() {
+        let accent: Accent = jangdanAccentList[self.currentBeatIndex % jangdanAccentList.count]
+        self.soundManager.beep(accent)
+        self.currentBeatIndex += 1
+    }
+}
