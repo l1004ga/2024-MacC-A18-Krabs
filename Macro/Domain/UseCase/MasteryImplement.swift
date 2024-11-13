2230f9ca8f9f8492a1c846a94ef5f6e8a8368134
jina <l1004ga@gmail.com>
2024-11-13 20:57:58 +0900

    [feat] #137 재생중 외부로부터 중단 발생 시 SMasteryScore 업데이트 후 종료 기능 추가

--- Macro/Domain/UseCase/MasteryImplement.swift
+++ Macro/Domain/UseCase/MasteryImplement.swift
@@ -13,17 +13,25 @@ class MasteryImplement {
     private var masteryRepository: MasteryRepository
     private var startTime: Date!
     private var jangdan: Jangdan?
+    private var isRecording: Bool
     
     private var cancelbag: Set<AnyCancellable> = []
     
     init(jangdanRepository: JangdanRepository, masteryRepository: MasteryRepository) {
         self.jangdanRepository = jangdanRepository
         self.masteryRepository = masteryRepository
+        self.isRecording = false
         self.jangdanRepository.jangdanPublisher.sink { [weak self] jangdanEntity in
             guard let self else { return }
             self.jangdan = Jangdan(rawValue: jangdanEntity.name)
         }
         .store(in: &cancelbag)
+    }
+    
+    deinit {
+        if isRecording {
+            self.stopRecord()
+        }
     }
 }
 
@@ -34,6 +42,7 @@ extension MasteryImplement: MasteryUseCase {
     }
     
     func startRecord() {
+        self.isRecording = true
         self.startTime = .now
     }
     
@@ -41,5 +50,6 @@ extension MasteryImplement: MasteryUseCase {
         let practiceTime = startTime.timeIntervalSinceNow
         guard let jangdan = self.jangdan else { return }
         self.masteryRepository.updateMastary(practiceTime: Int(practiceTime))
+        self.isRecording = false
     }
 }
