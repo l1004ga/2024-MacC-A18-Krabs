52f1a9440305cffd87f994d4b5e6304df90f10c7
jina <l1004ga@gmail.com>
2024-11-19 14:48:07 +0900

    [feat] #139 소박 SegementView 구현

---
+++ Macro/Screen/MetronomeScreen/SobakSegmentView.swift
@@ -0,0 +1,31 @@
+//
+//  SobakSegmentView.swift
+//  Macro
+//
+//  Created by leejina on 11/19/24.
+//
+
+import SwiftUI
+
+struct SobakSegmentsView: View {
+    var body: some View {
+        HStack(spacing: 2) {
+            Rectangle()
+                .foregroundStyle(.sobakSegmentDaebak)
+            Rectangle()
+                .foregroundStyle(.frame)
+        }
+        .background(.bakBarBorder)
+        .frame(height: 20)
+        .overlay {
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(.bakBarBorder, lineWidth: 2)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+    }
+}
+
+#Preview {
+    SobakSegmentsView()
+}
+
