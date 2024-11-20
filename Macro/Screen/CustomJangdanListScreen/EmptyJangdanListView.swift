dd64a316723e3f33b4b636dea5ed5b8dee3e360d
YunKi-H <yunki.dev@gmail.com>
2024-11-20 23:54:47 +0900

    [refactor] #169 EmptyJangdanListView 레이아웃 변경

--- Macro/Screen/CustomJangdanListScreen/EmptyJangdanListView.swift
+++ Macro/Screen/CustomJangdanListScreen/EmptyJangdanListView.swift
@@ -9,38 +9,34 @@ import SwiftUI
 
 struct EmptyJangdanListView: View {
     var body: some View {
-        ZStack {
-            HStack() {
-                VStack(alignment: .leading, spacing: 8) {
-                    
-                    Text("아직 저장한 장단이 없어요")
-                        .font(.Subheadline_R) // SwiftUI 문법에서 소문자로 시작
-                        .foregroundColor(.textTertiary)
-                    
-                    Text("장단 만들러 가기")
-                        .font(.Title3_R)
-                        .foregroundColor(.textSecondary)
-                    
-                }
-                    Spacer()
-                    
-                    Image(systemName: "chevron.right")
-                        .fontWeight(.semibold)
-                        .frame(width: 20, height: 22)
-                        .foregroundColor(.textTertiary)
+        HStack {
+            VStack(alignment: .leading, spacing: 8) {
+                
+                Text("아직 저장한 장단이 없어요")
+                    .font(.Subheadline_R) // SwiftUI 문법에서 소문자로 시작
+                    .foregroundColor(.textTertiary)
+                
+                Text("장단 만들러 가기")
+                    .font(.Title3_R)
+                    .foregroundColor(.textSecondary)
                 
             }
-            .padding(.horizontal, 20)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.backgroundCard)
-            .cornerRadius(16)
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                .fontWeight(.semibold)
+                .frame(width: 20, height: 22)
+                .foregroundColor(.textTertiary)
+            
         }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 16)
+        .background(.backgroundCard)
+        .cornerRadius(16)
     }
 }
 
-struct NoCustomJangdanComponentView_Previews: PreviewProvider {
-    static var previews: some View {
-        EmptyJangdanListView()
-            .frame(width: 361, height: 97)
-    }
+#Preview {
+    EmptyJangdanListView()
 }
