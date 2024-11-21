a53f1b0b86edff0b4bd618025e21306dcfecc969
YunKi-H <yunki.dev@gmail.com>
2024-11-21 12:45:50 +0900

    [feat] #169 JangdanTypeSelectView 개요 작성

---
+++ Macro/Screen/CustomJangdanListScreen/JangdanTypeSelectView.swift
@@ -0,0 +1,41 @@
+//
+//  JangdanTypeSelectView.swift
+//  Macro
+//
+//  Created by Yunki on 11/21/24.
+//
+
+import SwiftUI
+
+struct JangdanTypeSelectView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("만들 장단의\n종류를 선택해주세요.")
+                .multilineTextAlignment(.leading)
+                .font(.Title1_R)
+                .foregroundStyle(.textDefault)
+                .padding(.leading, 20)
+                
+            
+            List {
+                // TODO: - 악기 종류에 따라 표시되는 장단 리스트 달라야함
+                ForEach(Jangdan.allCases, id: \.self) { jangdan in
+                    NavigationLink {
+                        // MARK: - CustomJangdanCreateView 로 이동
+                    } label: {
+                        Text(jangdan.name)
+                            .foregroundStyle(.primary)
+                            .padding(.vertical, 9)
+                    }
+                    
+                }
+                
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    JangdanTypeSelectView()
+}
