5bf29d24b1f3cf2661d3559f458e06bea83e716c
jina <l1004ga@gmail.com>
2024-12-02 22:04:22 +0900

    [fix] #282 장단 내보내기 시 이전 장단 이름 남아있는 오류 해결

--- Macro/Screen/MetronomeScreen/MetronomeView.swift
+++ Macro/Screen/MetronomeScreen/MetronomeView.swift
@@ -68,6 +68,7 @@ struct MetronomeView: View {
                                 } completion: {
                                     self.toastAction = false
                                     self.toastOpacity = 1
+                                    inputCustomJangdanName = ""
                                 }
                             }
                     }
