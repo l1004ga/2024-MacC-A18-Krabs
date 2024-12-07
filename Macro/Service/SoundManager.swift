60a42089d0395f15cc818dff996648cc20a489f1
jina <l1004ga@gmail.com>
2024-12-07 18:35:37 +0900

    [feat] #293 다른 앱 Sound 재생 중에 동시에 우리 앱에서 사운드 재생되도록 구현

--- Macro/Service/SoundManager.swift
+++ Macro/Service/SoundManager.swift
@@ -23,7 +23,8 @@ class SoundManager {
 
         // AudioSession 설정
         do {
-            try self.audioSession.setCategory(.playback, mode: .default)
+//            try self.audioSession.setCategory(.playback, mode: .default)
+            try self.audioSession.setCategory(.playback, options: [.mixWithOthers])
             try self.audioSession.setActive(true)
         } catch {
             print("SoundManager: 오디오 세션 설정 중 에러 발생 - \(error)")
