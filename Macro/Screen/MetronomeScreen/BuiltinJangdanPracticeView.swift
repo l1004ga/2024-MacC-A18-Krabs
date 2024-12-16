84699e2950b3977948dd61c80dd0914f6ab12c2b
YunKi-H <yunki.dev@gmail.com>
2024-12-16 14:51:48 +0900

    [refactor] #222 BuiltInJangdanPracticeView에서 MetronomeView 분리

--- Macro/Screen/MetronomeScreen/BuiltinJangdanPracticeView.swift
+++ Macro/Screen/MetronomeScreen/BuiltinJangdanPracticeView.swift
@@ -6,11 +6,10 @@
 //
 
 import SwiftUI
-import Combine
 
 struct BuiltinJangdanPracticeView: View {
     
-    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Environment(\.dismiss) var dismiss
     @State var viewModel: MetronomeViewModel
     
     @State private var appState: AppState = DIContainer.shared.appState
@@ -31,75 +30,14 @@ struct BuiltinJangdanPracticeView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
-                VStack(spacing: 12) {
-                    HanbaeBoardView(
-                        jangdan: viewModel.state.jangdanAccent,
-                        isSobakOn: self.viewModel.state.currentJangdanType?.sobakSegmentCount == nil ? viewModel.state.isSobakOn : false,
-                        isPlaying: viewModel.state.isPlaying,
-                        currentRow: viewModel.state.currentRow,
-                        currentDaebak: viewModel.state.currentDaebak,
-                        currentSobak: viewModel.state.currentSobak
-                    ) { row, daebak, sobak, newAccent in
-                        withAnimation {
-                            viewModel.effect(action: .changeAccent(row: row, daebak: daebak, sobak: sobak, newAccent: newAccent))
-                        }
-                    }
-                    if let sobakSegmentCount = self.viewModel.state.currentJangdanType?.sobakSegmentCount {
-                        SobakSegmentsView(sobakSegmentCount: sobakSegmentCount, currentSobak: self.viewModel.state.currentSobak, isPlaying: self.viewModel.state.isPlaying, isSobakOn: self.viewModel.state.isSobakOn)
-                            .padding(.bottom, -4)
-                    }
-                }
-                .frame(height: 372)
-                .padding(.horizontal, 8)
-                if toastAction {
-                    Text("'\(inputCustomJangdanName)' 내보내기가 완료되었습니다.")
-                        .font(.Body_R)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 16)
-                        .foregroundStyle(Color.white)
-                        .background(.black.opacity(0.8))
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .opacity(self.toastOpacity)
-                        .onAppear {
-                            withAnimation(.easeIn(duration: 3)) {
-                                self.toastOpacity = 0
-                            } completion: {
-                                self.toastAction = false
-                                self.toastOpacity = 1
-                            }
-                        }
-                }
-            }
-            if let sobakSegmentCount = self.viewModel.state.currentJangdanType?.sobakSegmentCount {
-                ViewSobakToggleView(isSobakOn: $isSobakOn)
-                    .padding(.bottom, 16)
-            } else {
-                ListenSobakToggleView(isSobakOn: $isSobakOn)
-                    .padding(.bottom, 16)
-            }
-            MetronomeControlView(viewModel: viewModel)
-        }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            self.viewModel.effect(action: .disableEstimateBpm)
-        }
-        
-        .task {
-            self.viewModel.effect(action: .selectJangdan(selectedJangdanName: self.jangdanName))
-            self.isSobakOn = self.viewModel.state.isSobakOn
-        }
-        .onChange(of: isSobakOn) {
-            self.viewModel.effect(action: .changeSobakOnOff)
-        }
+        MetronomeView(viewModel: DIContainer.shared.metronomeViewModel, jangdanName: jangdanName)
         .navigationBarBackButtonHidden(true)
         .toolbar {
             // 뒤로가기 chevron
             ToolbarItem(placement: .navigationBarLeading) {
                 Button(action: {
                     self.viewModel.effect(action: .stopMetronome)
-                    presentationMode.wrappedValue.dismiss()
+                    self.dismiss()
                 }) {
                     Image(systemName: "chevron.backward")
                         .aspectRatio(contentMode: .fit)
