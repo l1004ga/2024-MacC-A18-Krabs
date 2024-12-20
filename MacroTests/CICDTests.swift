bf275c7fa7fd240d1f7451843beb434d65e0d06f
jina <l1004ga@gmail.com>
2024-12-20 21:38:12 +0900

    [test] CICD Tests 생성

---
+++ MacroTests/CICDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTests.swift
+//  MacroTests
+//
+//  Created by leejina on 12/20/24.
+//
+
+import Testing
+
+struct CICDTests {
+
+    @Test func cicdTest() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        #expect(true)
+    }
+
+}
