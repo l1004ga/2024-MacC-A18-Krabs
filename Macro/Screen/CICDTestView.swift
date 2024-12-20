cbf83cdb3478c9a0245187c7c4ced79f626baa2b
jina <l1004ga@gmail.com>
2024-12-20 21:42:20 +0900

    [test] CICDTestView 생성 및 develop 계정에 workflow 트리거 확인

---
+++ Macro/Screen/CICDTestView.swift
@@ -0,0 +1,18 @@
+//
+//  CICDTestView.swift
+//  Macro
+//
+//  Created by leejina on 12/20/24.
+//
+
+import SwiftUI
+
+struct CICDTestView: View {
+    var body: some View {
+        Text("지나가 테스트중입니다.")
+    }
+}
+
+#Preview {
+    CICDTestView()
+}
